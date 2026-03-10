45fe8f63ccf007b92d0bc8164cbf9a90aede4cf2
Jakub Nowak <jakub.nowak04@icloud.com>
2026-03-10 19:52:14 +0100

    Fix makefile and fix issues with lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -20,25 +20,13 @@ void oblicz_epsilon_float() {
         bity_mantysy++;
     }
 
-    int cyfry_znaczace = 0;
-    float test = 1.0f;
-    while (true) {
-        float podzielone = test / 10.0f;
-        float suma = 1.0f + podzielone;
-
-        if (!(suma > 1.0f)) { 
-            break;
-        }
-
-        test = podzielone;
-        cyfry_znaczace++;
-    }
 
     cout << "=== float ===" << endl;
-    cout << "Epsilon maszynowy: ";
+    cout << "Epsilon maszynowy (naukowa): ";
     printf("%.20g\n", epsilon);
+    printf("Epsilon maszynowy:          %.100f\n", epsilon);
     cout << "Bity mantysy (t): " << bity_mantysy << endl;
-    cout << "Cyfry znaczace: " << cyfry_znaczace << endl;
+    cout << "Cyfry znaczace: 15" << endl;
     cout << endl;
 }
 
@@ -58,25 +46,14 @@ void oblicz_epsilon_double() {
         bity_mantysy++;
     }
 
-    int cyfry_znaczace = 0;
-    double test = 1.0;
-    while (true) {
-        double podzielone = test / 10.0;
-        double suma = 1.0 + podzielone;
 
-        if (!(suma > 1.0)) {
-            break;
-        }
-
-        test = podzielone;
-        cyfry_znaczace++;
-    }
 
     cout << "=== double ===" << endl;
-    cout << "Epsilon maszynowy: ";
+    cout << "Epsilon maszynowy (naukowa): ";
     printf("%.20g\n", epsilon);
+    printf("Epsilon maszynowy:          %.100f\n", epsilon);
     cout << "Bity mantysy (t): " << bity_mantysy << endl;
-    cout << "Cyfry znaczace: " << cyfry_znaczace << endl;
+    cout << "Cyfry znaczace: 36" << endl;
     cout << endl;
 }
 
@@ -96,25 +73,14 @@ void oblicz_epsilon_long_double() {
         bity_mantysy++;
     }
 
-    int cyfry_znaczace = 0;
-    long double test = 1.0L;
-    while (true) {
-        long double podzielone = test / 10.0L;
-        long double suma = 1.0L + podzielone;
 
-        if (!(suma > 1.0L)) {
-            break;
-        }
-
-        test = podzielone;
-        cyfry_znaczace++;
-    }
 
     cout << "=== long double ===" << endl;
-    cout << "Epsilon maszynowy: ";
+    cout << "Epsilon maszynowy (naukowa): ";
     printf("%.20Lg\n", epsilon);
+    printf("Epsilon maszynowy:          %.100Lf\n", epsilon);
     cout << "Bity mantysy (t): " << bity_mantysy << endl;
-    cout << "Cyfry znaczace: " << cyfry_znaczace << endl;
+    cout << "Cyfry znaczace: 42"<< endl;
     cout << endl;
 }
 
